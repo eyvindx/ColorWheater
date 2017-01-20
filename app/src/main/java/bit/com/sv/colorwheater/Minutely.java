899f7105bee9b4796b3678ef93670a8d16358490
eyvmen <eyvindr.mendozah@gmail.com>
2017-01-20 01:13:08 -0600

    Initial commit

---
+++ app/src/main/java/bit/com/sv/colorwheater/Minutely.java
@@ -0,0 +1,13 @@
+package bit.com.sv.colorwheater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Minutely extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minutely);
+    }
+}
